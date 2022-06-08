72fa15c1bff14de53fc806af6fa25d79d5e29092
Asep Saepul Rohmat <asepsaepulr88@gmail.com>
2022-06-08 10:48:24 +0700

    Category, Author, Publisher

---
+++ app/Http/Controllers/PublisherController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Publisher;
+
+class PublisherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $publisher = Publisher::paginate(10);
+        return view('publisher.index',compact('publisher'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('publisher.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name_publisher' => 'required',
+        ]);
+        Publisher::create($request->all());
+
+        return redirect()->route('publisher.index')->with('succes','Data Berhasil di Input');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $publisher = Publisher::findOrFail($id);
+        return view('publisher.edit', compact('publisher'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,Publisher $publisher)
+    {
+        $request->validate([
+            'name_publisher' => 'required',
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('publisher.index')->with('success','Publisher Berhasil di Update');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(!Publisher::destroy($id)) return redirect()->back();
+        return redirect()->route('publisher.index')->with('success','Publisher Berhasil di Hapus');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,8 +22,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', [AuthController::class, 'indexLogin'])->name('login');
-Route::get('register', [AuthController::class, 'indexRegister'])->name('register');
-Route::get('admin',  [AdminController::class, 'index'])->name('admin');
-Route::get('admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
-Route::get('{page}', [PageController::class, 'indexAdmin'])->name('admin.page.index');
+// Route::get('login', [AuthController::class, 'indexLogin'])->name('login');
+// Route::get('register', [AuthController::class, 'indexRegister'])->name('register');
+// Route::get('admin',  [AdminController::class, 'index'])->name('admin');
+// Route::get('admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+// Route::get('{page}', [PageController::class, 'indexAdmin'])->name('admin.page.index');
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+Route::get('register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+Route::get('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+
+Route::get('password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+Route::post('password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class, 'confirm']);
+
+Route::get('email/verify', [App\Http\Controllers\Auth\VerificationController::class, 'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [App\Http\Controllers\Auth\VerificationController::class, 'verify'])->name('verification.verify');
+Route::post('email/resend', [App\Http\Controllers\Auth\VerificationController::class, 'resend'])->name('verification.resend');
+
+//Feature
+//Route::get('/category/index', [App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
+Route::resource('category', CategoryController::class);
+Route::resource('author', AuthorController::class);
+Route::resource('publisher', PublisherController::class);
