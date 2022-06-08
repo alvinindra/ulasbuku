b2862399f5d2b8e84591b074526f22f49cb1aa60
Alvin Indra <alvinindrap@gmail.com>
2022-06-08 12:49:23 +0700

    style: structuring scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,11 @@
     <title>UlasBuku</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
